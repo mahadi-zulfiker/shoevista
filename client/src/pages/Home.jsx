eeefa277282d51807e161d50d14de70bbb3be499
mahadi-zulfiker <mahade.adib45@gmail.com>
2026-02-12 18:11:23 +0600

    done

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Carousel from "../components/Carousel";
+import ShopBy from "../components/ShopBy";
+import GenInfo, { Brands } from "../components/GenInfo";
+
+const Home = () => {
+  return (
+    <div className="max-w-screen-xl xs:w-[95vw] xs:max-w-[95vw] md:w-full mx-auto ">
+      <Carousel />
+      <GenInfo />
+      <Brands />
+      <div className="md:w-full md:max-w-full xs:mx-2  sm:mx-auto ">
+        <div className="prose prose-2xl">
+          <Brands title="Best Sellers" />
+          <ShopBy filter="bestSellers" />
+        </div>
+        <div className="prose prose-2xl">
+          <Brands title="Top Rated" />
+          <ShopBy filter="topRated" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
